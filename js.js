2bc755ffef6b0cbb0a0ae09ed936f1936481f29e
kishore163 <kishorekumasrsh715@gmail.com>
2025-03-07 15:44:41 +0530

    changes in js file

--- js.js
+++ js.js
@@ -2,8 +2,41 @@ const prompt =require("prompt-sync")();
 
 const deposit=()=>{
     
-    const amount= prompt("Enter the deposit Amount: ");
-    console.log(typeof amount);
+    const checkamount= prompt("Enter the deposit Amount: ");
+    const amount=parseFloat(checkamount);
+    if(isNaN(amount)|| amount<=0){
+        console.log("Invalid amount , enter a positive number");
+    }
+    else{
+        return amount;
+    }
+    
+}
+const getnumberofLines=()=>{
+    
+    const lines= prompt("Enter the number of lines between 1-3: ");
+    const numberofLines=parseFloat(lines);
+    if(isNaN(numberofLines)|| numberofLines<=0 || numberofLines>3){
+        console.log("Invalid linesr");
+    }
+    else{
+        return numberofLines;
+    }
+    
 }
 
-deposit();
+const getBet=(balance)=>{
+    const getuserBet=prompt("Enter the bet amount: ");
+    const userBet=parseFloat(getuserBet);
+    if(userBet>balance || userBet<=0){
+        console.log("Invalid bet amount");
+    }
+    else{
+        return userBet;
+    }
+
+}
+
+let depositAmount=deposit();
+const numberofLines=getnumberofLines();
+const userBet= getBet(depositAmount);
